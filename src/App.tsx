45238d5a6691802ad1580a5465f8104ab05e50ad
Serhii Korniichuk <skorniychyk@gmail.com>
2022-12-15 17:23:42 +0200

    ADD: chakra theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { Button, ChakraProvider } from '@chakra-ui/react'
 import './App.css'
+import { theme } from './common/theme'
 
 function App() {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Button>Color Mood</Button>
     </ChakraProvider>
   )
---
+++ src/common/theme.ts
@@ -0,0 +1,8 @@
+import { extendTheme } from '@chakra-ui/react'
+
+const palette = {
+  white: '#FFFFFF',
+  black: '#000000',
+}
+
+export const theme = extendTheme({ palette })
