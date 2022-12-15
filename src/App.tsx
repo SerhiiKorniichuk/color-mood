e6192279067d261471659a04079dabe90f547daf
Serhii Korniichuk <skorniychyk@gmail.com>
2022-12-15 17:34:40 +0200

    ADD: styled-components library

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import { Button, ChakraProvider } from '@chakra-ui/react'
+import styled from 'styled-components'
 import './App.css'
 import { theme } from './common/theme'
+
+const StyledButton = styled(Button)`
+  &.chakra-button {
+    font-size: 20px;
+  }
+`
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Button>Color Mood</Button>
+      <StyledButton>Color Mood</StyledButton>
     </ChakraProvider>
   )
 }
